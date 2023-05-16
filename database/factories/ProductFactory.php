85447f069a40e6a791ea2ca07f57df0d1c5d5457
Anamika Kumar <anamika.panwar@gmail.com>
2023-05-16 10:03:29 +0200

    project

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Producty>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'category_id'=>rand(1,10),
+            'name'=>$this->faker->text(maxNbChars:20),
+            'price'=>rand(1000,9999)
+        ];
+    }
+}
